f59dae74c493ce51351b805ad49070cbb42edf6d
Tim DeCillis <timdecillis@gmail.com>
2023-07-13 22:29:55 -0700

    Add files from corrupt branch

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+const db = require('./databas.js');
+const router = require('express').Router();
+const {getReviews, getReviewMeta, addReview, addHelpful} = require('./database');
+
+app.use(express.json());
+app.use('/reviews', router);
+
+router.get('/', (req, res)=> {
+  getReviews()
+    .then((reviews) => {
+      res.status(200).send(reviews);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.get('/meta', (req, res) => {
+  getReviewMeta()
+    .then((metaData) => {
+      res.status(200).send(metaData);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.post('/', (req, res) => {
+  addReview(req)
+    .then(() => {
+      res.status(201).send();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.put('/', (req, res) => {
+  addHelpful()
+    .then(() => {
+      res.status(204).send();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+let port = 3000;
+
+app.listen(port, function() {
+  console.log(`listening on port ${port}`);
+});
+
--- schema/load.js
+++ schema/load.js
@@ -1,17 +1,58 @@
-const fs = require('fs')
-const path = require('path')
-const { Pool, Client} = require('pg')
-const copyFrom = require('pg-copy-streams').from
-const config = require('./config.json')
+const fs = require('fs');
+const path = require('path');
+const { Pool, Client } = require('pg');
+const { host, user, pw, db, port } = require('../config.js');
 
 // inputfile & target table
-var inputFile = path.join(__dirname, '/data/customer.csv')
-var table = 'usermanaged.customers'
+const dbConfig = {
+  host: host,
+  user: user,
+  pw: pw,
+  database: db,
+  port: port
+};
 
-// Getting connectin parameters from config.json
-const host = config.host
-const user = config.user
-const pw = config.pw
-const db = config.db
-const port = config.port
-const conString = `postgres://${user}:${pw}@${host}:${port}/${db}`
+const paths = {
+  schema: path.join(__dirname, '/createTables.sql'),
+  chars: path.join(__dirname, '/data/characteristics.csv'),
+  charsValues: path.join(__dirname, '/data/characteristic_reviews.csv'),
+  reviews: path.join(__dirname, '/data/reviews.csv'),
+  reviews_photos: path.join(__dirname, '/data/reviews_photos.csv')
+};
+
+const queries = {
+  createTables: fs.readFileSync(paths.schema, 'utf-8'),
+  chars: `COPY characteristics FROM '${paths.chars}' DELIMITER ',' CSV HEADER;`,
+  charsValues: `COPY characteristic_values FROM '${paths.charsValues}' DELIMITER ',' CSV HEADER;`,
+  reviews: `COPY reviews FROM '${paths.reviews}' DELIMITER ',' CSV HEADER;`,
+  reviews_photos: `COPY reviews_photos FROM '${paths.reviews_photos}' DELIMITER ',' CSV HEADER;`
+};
+
+
+const createTables = async () =>{
+  const client = new Client(dbConfig);
+
+  try {
+
+    await client.connect();
+    console.log('Connected to the database.');
+
+    await client.query(queries.createTables);
+    console.log(`Tables created successfully.`);
+
+    await client.query(queries.chars);
+    await client.query(queries.reviews);
+    await client.query(queries.reviews_photos);
+    await client.query(queries.charsValues);
+  } catch (error) {
+
+    await client.query('ROLLBACK');
+    console.error('Error creating table:', error);
+  } finally {
+
+    await client.end();
+    console.log('Disconnected from the database.');
+  }
+}
+
+createTables();
