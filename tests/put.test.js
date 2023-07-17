df46d8824cb145691fc785e59ad2063fd85f4cc1
Tim DeCillis <timdecillis@gmail.com>
2023-07-16 20:49:46 -0700

    Added basic status code and content type tests for get, post, and put methods

---
+++ tests/put.test.js
@@ -0,0 +1,20 @@
+const request = require('supertest');
+const baseURL = 'localhost:3000/';
+
+describe('PUT, /reviews', () => {
+  let response;
+  beforeAll(async () => {
+    response = await request(baseURL).put('reviews/1275918/helpful');
+  });
+  it('responds with 204 status code', () => {
+    expect(response.status).toBe(204);
+  });
+  it ('should not throw an error', () => {
+    expect(response.body.error).toBeUndefined();
+  });
+  it ('should successfully update the review entry', async () => {
+    // let query = await request(baseURL).get('reviews/1280253');
+    // console.log(query._body.results);
+    // expect(response._body.helpful).toBe();
+  });
+});
