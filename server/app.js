1fdbb2a98118777be56bb6e4f7cd7efb31790d3c
Tim DeCillis <timdecillis@gmail.com>
2024-03-04 11:21:21 -0800

    cleaned up app

--- server/app.js
+++ server/app.js
@@ -1,29 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const db = require('./database.js');
-const router = require('express').Router();
-const morgan = require('morgan');
-const {getReviews, getReviewMeta, addReview, addHelpful, getReview} = require('./database');
+const db = require("./database.js");
+const router = require("express").Router();
+const morgan = require("morgan");
+const {
+  getReviews,
+  getReviewMeta,
+  addReview,
+  addHelpful,
+  getReview,
+} = require("./database");
 
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
-app.use('/reviews', router);
+app.use("/reviews", router);
 
-router.get('/', (req, res)=> {
+router.get("/", (req, res) => {
   // eslint-disable-next-line camelcase
 
-  let {product_id, sort, count, page} = req.query;
+  let { product_id, sort, count, page } = req.query;
   page = page || 1;
-  if (typeof(count) !== 'number') {
+  if (typeof count !== "number") {
     count = 5;
   }
-  sort = typeof sort !== 'undefined' ? sort : 'relevant';
+  sort = typeof sort !== "undefined" ? sort : "relevant";
   getReviews(product_id, page, count, sort)
     .then((reviews) => {
       res.status(200).send(reviews);
@@ -33,9 +39,9 @@ router.get('/', (req, res)=> {
     });
 });
 
-router.get('/meta', (req, res) => {
+router.get("/meta", (req, res) => {
   // eslint-disable-next-line camelcase
-  let {product_id} = req.query;
+  let { product_id } = req.query;
   getReviewMeta(product_id)
     .then((metaData) => {
       res.status(200).send(metaData);
@@ -45,7 +51,7 @@ router.get('/meta', (req, res) => {
     });
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   let review = req.body;
   addReview(review)
     .then(() => {
@@ -56,8 +62,8 @@ router.post('/', (req, res) => {
     });
 });
 
-router.put('/:review_id/helpful', (req, res) => {
-  let {review_id} = req.params;
+router.put("/:review_id/helpful", (req, res) => {
+  let { review_id } = req.params;
   addHelpful(review_id)
     .then(() => {
       res.status(204).send();
@@ -67,8 +73,8 @@ router.put('/:review_id/helpful', (req, res) => {
     });
 });
 
-router.get('/:review_id/', (req, res) => {
-  let {review_id} = req.params;
+router.get("/:review_id/", (req, res) => {
+  let { review_id } = req.params;
   getReview(review_id)
     .then((review) => {
       res.status(200).send(review);
